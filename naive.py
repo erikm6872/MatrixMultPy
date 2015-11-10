44c1ac4bb5b03782401c08794eb296f0f609c1bb
Erik McLaughlin <erikm6872@gmail.com>
2015-11-10 16:36:38 -0700

    updated size check

--- naive.py
+++ naive.py
@@ -16,11 +16,7 @@ def main(n):
     
 #Returns c, an nxn matrix that is the product of a and b
 def matrixMult(a,b):    
-    if len(a) != len(b) or len(a) != len(a[0]) or len(a[0]) != len(b[0]): #Check to make sure matrices are both nxn
-        print "Error: matrices are not the same size"   #Print error message if not
-        print "Matrix A: %dx%d" % (len(a), len(a[0]))
-        print "Matrix B: %dx%d" % (len(b), len(b[0]))
-    else:
+    if mo.matricesSameSize(a,b):
         n = len(a)
         
         c = [[0 for x in range(n)] for x in range(n)]
@@ -30,5 +26,8 @@ def matrixMult(a,b):
                 for k in range(n):
                     c[i][j] = c[i][j] + a[i][k] * b[k][j]
         return c
-        
+    else:
+        print "Error: matrices are not the same size"
+        print "Matrix A: %dx%d" % (len(a), len(a[0]))
+        print "Matrix B: %dx%d" % (len(b), len(b[0]))
 main(5) #Call main() with matrix size
